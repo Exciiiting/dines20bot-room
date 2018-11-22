a39b8491fa0950aed94fcf15d2cf3e03f8d4c304
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-11-22 03:42:48 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,30 +50,7 @@ console.log("Failed To Join:\n The Channel Type isn't \"text\"");
 
 
 
-const adminprefix = "1";
-const devs = ['323160008411971585','323160008411971585'];
-client.on('message', message => {
-  var argresult = message.content.split(`).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'ply')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(${argresult} تم تغيير بلاينق البوت إلى )
-} else 
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(${argresult} : تم تغيير أسم البوت إلى`)
-return message.reply("لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . ");
-} else
-  if (message.content.startsWith(adminprefix + 'setavatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`${argresult} : تم تغير صورة البوت);
-      } else     
-if (message.content.startsWith(adminprefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/Weented");
-    message.channel.sendMessage(تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
-});
+
 
 
  
